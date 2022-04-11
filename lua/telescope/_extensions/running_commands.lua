2c08687aa35e1618b2949ec68d5ee1cb6dd5e43d
Keyv Chan <keyvchan@gmail.com>
2022-04-11 13:48:02 +0800

    chore: cleanup unused files

--- lua/telescope/_extensions/running_commands.lua
+++ lua/telescope/_extensions/running_commands.lua
@@ -6,79 +6,12 @@ end
 local fb_finders = require("telescope._extensions.running_commands.finders")
 local fb_picker = require("telescope._extensions.running_commands.picker")
 
-local finders = require("telescope.finders")
-local action_state = require("telescope.actions.state")
-local actions = require("telescope.actions")
-local action_set = require("telescope.actions.set")
-
-local pconf = {
-	mappings = {},
-	attach_mappings = function(prompt_bufnr, _)
-		action_set.select:replace(function()
-			local entry = action_state.get_selected_entry()
-			local current_picker = action_state.get_current_picker(prompt_bufnr)
-			local finder = current_picker.finder
-			finder.files = true
-			finder.path = entry.value
-			local current_title = current_picker.results_border._border_win_options.title
-			local data = vim.fn.getcompletion(entry.value .. " ", "cmdline")
-			if vim.tbl_isempty(data) then
-				actions.close(prompt_bufnr)
-				vim.api.nvim_command(current_title .. " " .. entry.value)
-			else
-				finder = finders.new_table({ results = data })
-				local new_title
-				if current_title == "Results" then
-					new_title = entry.value
-				else
-					new_title = current_title .. " " .. entry.value
-				end
-				current_picker.results_border:change_title(new_title)
-				current_picker:refresh(finder, { reset_prompt = true, multi = current_picker._multi })
-			end
-		end)
-		return true
-	end,
-}
-
-local fb_setup = function(opts)
-	-- TODO maybe merge other keys as well from telescope.config
-	pconf.mappings = vim.tbl_deep_extend("force", pconf.mappings, require("telescope.config").values.mappings)
-	pconf = vim.tbl_deep_extend("force", pconf, opts)
-end
+local fb_setup = function(opts) end
 
 local running_commands = function(opts)
 	opts = opts or {}
-	local defaults = (function()
-		if pconf.theme then
-			return require("telescope.themes")["get_" .. pconf.theme](pconf)
-		end
-		return vim.deepcopy(pconf)
-	end)()
 
-	if pconf.mappings then
-		defaults.attach_mappings = function(prompt_bufnr, map)
-			if pconf.attach_mappings then
-				pconf.attach_mappings(prompt_bufnr, map)
-			end
-			for mode, tbl in pairs(pconf.mappings) do
-				for key, action in pairs(tbl) do
-					map(mode, key, action)
-				end
-			end
-			return true
-		end
-	end
-
-	if opts.attach_mappings then
-		local opts_attach = opts.attach_mappings
-		opts.attach_mappings = function(prompt_bufnr, map)
-			defaults.attach_mappings(prompt_bufnr, map)
-			return opts_attach(prompt_bufnr, map)
-		end
-	end
-	local popts = vim.tbl_deep_extend("force", defaults, opts)
-	fb_picker(popts)
+	fb_picker(opts)
 end
 
 return telescope.register_extension({
--- lua/telescope/_extensions/running_commands/picker.lua
+++ lua/telescope/_extensions/running_commands/picker.lua
@@ -2,58 +2,50 @@ local pickers = require("telescope.pickers")
 local conf = require("telescope.config").values
 
 local fb_finder = require("telescope._extensions.running_commands.finders")
-
-local Path = require("plenary.path")
-local os_sep = Path.path.sep
+local themes = require("telescope.themes")
+local finders = require("telescope.finders")
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
+local action_set = require("telescope.actions.set")
 
 -- enclose in module for docgen
 local fb_picker = {}
 
--- try to get the index of entry of current buffer
-local get_selection_index = function(opts, results)
-	local buf_path = vim.api.nvim_buf_get_name(0)
-	local current_dir = Path:new(buf_path):parent():absolute()
-
-	if opts.path == current_dir then
-		for i, path_entry in ipairs(results) do
-			if path_entry.value == buf_path then
-				return i
-			end
-		end
-	end
-	return vim.F.if_nil(opts.default_selection_index, 1)
-end
-
 fb_picker.running_command = function(opts)
-	opts = opts or {}
+	opts = themes.get_dropdown(opts)
 
-	local cwd = vim.loop.cwd()
-	opts.depth = vim.F.if_nil(opts.depth, 1)
-	opts.cwd_to_path = vim.F.if_nil(opts.cwd_to_path, false)
-	opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or cwd
-	opts.path = opts.path and vim.fn.expand(opts.path) or opts.cwd
-	opts.files = vim.F.if_nil(opts.files, true)
-	opts.hide_parent_dir = vim.F.if_nil(opts.hide_parent_dir, false)
-	opts.select_buffer = vim.F.if_nil(opts.select_buffer, false)
 	opts.custom_prompt_title = opts.prompt_title ~= nil
-	opts.custom_results_title = opts.results_title ~= nil
-
-	local select_buffer = opts.select_buffer and opts.files
-	-- handle case that current buffer is a hidden file
-	opts.hidden = (select_buffer and vim.fn.expand("%:p:t"):sub(1, 1) == ".") and true or opts.hidden
-	local finder = fb_finder.finder(opts)
-	-- find index of current buffer in the results
-	if select_buffer then
-		finder._finder = finder:_browse_files()
-		opts.default_selection_index = get_selection_index(opts, finder.results)
-	end
 
 	pickers.new(opts, {
 		prompt_title = "Commands Avaiable",
-		results_title = "Results",
-		finder = finder,
-		previewer = conf.file_previewer(opts),
+		finder = fb_finder.finder(opts),
 		sorter = conf.file_sorter(opts),
+		attach_mappings = function(prompt_bufnr, _)
+			action_set.select:replace(function()
+				local entry = action_state.get_selected_entry()
+				local current_picker = action_state.get_current_picker(prompt_bufnr)
+				local finder = current_picker.finder
+				finder.files = true
+				finder.current_commands = entry.value
+				local current_title = current_picker.prompt_border._border_win_options.title
+				local data = vim.fn.getcompletion(entry.value .. " ", "cmdline")
+				if vim.tbl_isempty(data) then
+					actions.close(prompt_bufnr)
+					vim.api.nvim_command(current_title .. " " .. entry.value)
+				else
+					finder = finders.new_table({ results = data })
+					local new_title
+					if current_title == "Commands Avaiable" then
+						new_title = entry.value
+					else
+						new_title = current_title .. " " .. entry.value
+					end
+					current_picker.prompt_border:change_title(new_title)
+					current_picker:refresh(finder, { reset_prompt = true })
+				end
+			end)
+			return true
+		end,
 	}):find()
 end
 
