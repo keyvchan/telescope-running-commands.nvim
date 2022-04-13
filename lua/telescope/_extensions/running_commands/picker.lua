0ee913a849f53191557481533874f2c028876dd4
Keyv Chan <keyvchan@gmail.com>
2022-04-13 18:34:53 +0800

    fix: quit on wq! immeditly

--- lua/telescope/_extensions/running_commands/picker.lua
+++ lua/telescope/_extensions/running_commands/picker.lua
@@ -25,29 +25,36 @@ fb_picker.running_command = function(opts)
 				local entry = action_state.get_selected_entry()
 				local current_picker = action_state.get_current_picker(prompt_bufnr)
 				local finder = current_picker.finder
+				local current_commands
 				finder.files = true
 				if entry == nil then
-					finder.current_commands = action_state.get_current_line()
+					current_commands = action_state.get_current_line()
 				else
-					finder.current_commands = entry.value
+					current_commands = entry.value
 				end
+
 				local current_title = current_picker.prompt_border._border_win_options.title
-				local data = vim.fn.getcompletion(finder.current_commands .. " ", "cmdline")
+				local data = vim.fn.getcompletion(current_commands .. " ", "cmdline")
+
+				-- check last char is "!"
+				if current_commands:sub(-1) == "!" then
+					data = {}
+				end
 				if vim.tbl_isempty(data) then
 					actions.close(prompt_bufnr)
 					if current_title == "Commands Avaiable" then
 						-- local cmd = finder.current_commands
-						vim.api.nvim_command(finder.current_commands)
+						vim.api.nvim_command(current_commands)
 					else
-						vim.api.nvim_command(current_title .. " " .. entry.value)
+						vim.api.nvim_command(current_title .. " " .. current_commands)
 					end
 				else
 					finder = finders.new_table({ results = data })
 					local new_title
 					if current_title == "Commands Avaiable" then
-						new_title = entry.value
+						new_title = current_commands
 					else
-						new_title = current_title .. " " .. entry.value
+						new_title = current_title .. " " .. current_commands
 					end
 					current_picker.prompt_border:change_title(new_title)
 					current_picker:refresh(finder, { reset_prompt = true })
